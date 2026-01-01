8b19039c3bbab16aec8700ea034cf0c6b81b71be
Faraz Akhtar <farazakhtar019@gmail.com>
2026-01-01 23:59:58 +0530

    fix: backend url issur

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/api/auth/google";
+    window.location.href = `${import.meta.env.VITE_API_URL}/api/auth/google`;
   };
 
   return (
